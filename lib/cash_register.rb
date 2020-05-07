294f4dd758a76bf5dcd2d50bc6d94ec57fd5d724
Lon Bitner <lonbitner@gmail.com>
2020-05-07 02:43:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,31 +1,46 @@
 class CashRegister
-
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
+  attr_accessor :total, :discount, :items, :last_transaction
+  
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items = []
+    @cart = []
   end
+  
+  def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
 
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
+    @cart << item_info
+
+    @total += price * quantity
+    
+    @last_transaction_amount = @total
+    @total
+   
   end
-
+  
   def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
+    if @discount == 0
+        return "There is no discount to apply."
     end
+    self.total -= @total * @discount / 100
+    return "After the discount, the total comes to $#{@total}."
   end
-
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
+  
+  def items
+    item_names = []
+    @cart.each do | item_info |
+      for qty in 1..item_info[:quantity] 
+        item_names << item_info[:name]
+      end 
+    end 
+    item_names
+  end
+  
+  def void_last_transaction()
+    @total -= @last_transaction_amount
   end
 end
