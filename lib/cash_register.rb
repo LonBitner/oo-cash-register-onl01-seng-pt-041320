f3ad21b7beeaa0309ccd743499c83386417ba87a
Lon Bitner <lonbitner@gmail.com>
2020-05-07 02:05:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,9 +12,11 @@ class CashRegister
   end
   
   def apply_discount
+    if @discount == 0
+        return "There is no discount to apply."
     self.total -= @total * @discount / 100
     return "After the discount, the total comes to $800."
-    return "There is no discount to apply."
+
   end
   
 end
